b33cbad63b1d4130c20a0c2217ecd0c20445be41
Andrew <kusooyarou@bk.ru>
2024-05-02 15:34:59 +0300

    edded PWM?

---
+++ NewButton/ButtonEmulator/NoTaskManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace ButtonEmulator
+{
+    public class NoTaskManager
+    {
+        public NoTaskManager() : this("ButtonEmulator")
+        {
+        }
+
+        public NoTaskManager(string name)
+        {
+            Name = name;
+        }
+
+        private string Name { get; }
+
+        public void CheckProcess()
+        {
+            int processCount = 0;
+            foreach(Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName == Name)
+                {
+                    processCount++;
+                }
+            }
+            if (processCount < 2)
+            {
+                if (!Debugger.IsAttached)
+                {
+                    //Process.Start(Name);  
+                }
+            }        
+        }       
+    }
+}
